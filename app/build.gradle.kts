68c55f54d342b721a559c361cbff565555fc92ab
djnixy <nikiakbar@gmail.com>
2025-12-29 16:22:10 +0700

    feat: Centralize app versioning in `gradle.properties` and update release workflow to use it and send release URL to Telegram.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.vuvur"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = project.property("app.versionCode").toString().toInt()
+        versionName = project.property("app.versionName").toString()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
